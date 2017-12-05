07c412af17fb024873bcf95ad13841c3a285529f
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2017-12-05 16:18:49 -0700

    This is the beginning of my project

---
+++ hey-teacher-api/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var socket = require('socket.io');
+
+var app = express();
+
+var server = app.listen(8080, '0.0.0.0', function(){
+    console.log("Listening on port 8080");
+});
+
+var io = socket(server);
+
+io.on('connection', function(socket){
+   console.log('made socket connection');
+});
