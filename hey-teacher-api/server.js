822299ffaf6334589ba1f6b7904493f1f182a983
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2017-12-05 22:05:51 -0700

    Testing git pulling stuff

--- hey-teacher-api/server.js
+++ hey-teacher-api/server.js
@@ -5,6 +5,7 @@ var app = express();
 
 var server = app.listen(8080, '0.0.0.0', function(){
     console.log("Listening on port 8080");
+    console.log("Testing Git stuff out!!");
 });
 
 var io = socket(server);
