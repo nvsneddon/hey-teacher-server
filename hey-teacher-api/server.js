c0b7e5da0fe113c57cfe1c77de75920ecee0ba68
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2017-12-11 17:47:37 -0700

    Made rooms into objects and made other changes while connecting to the server from the client.

--- hey-teacher-api/server.js
+++ hey-teacher-api/server.js
@@ -3,7 +3,7 @@ var socket = require('socket.io');
 
 var app = express();
 
-var openRooms = [];
+var roomObjects = [];
 
 var server = app.listen(8080, '0.0.0.0', function(){
     console.log("Listening on port 8080");
@@ -17,12 +17,43 @@ function disconnectRoom(roomNr){
   //todo Disconnect others in room from the room
 }
 
+
+/*
+*Generates a random number and checks to see if the number happens to be used already.
+Hence the need for a do while loop.
+*/
+function generateRoomCode(){
+  do{
+    var rmCode = Math.floor(Math.random() * 10000);
+  } while (function(){
+    for(x in roomObjects){
+      if (x.room_id === rmCode) return true;
+    }
+    return false;
+  });
+  return rmCode;
+}
+
+function RoomObject(var roomCode, var name){
+  this.room_id = roomCode;
+  this.teacherName = name;
+  this.waitlist = [];
+}
+
 io.on('connection', function(socket){
    console.log('made socket connection');
-   socket.on('teacher-room', function(room){
+  /* socket.on('teacher-room', function(room){
      socket.join(room);
      openRooms.push(room);
      console.log("New room has been created: " + room);
+   });*/
+   socket.on('teacher-connect', function(name){
+     var roomCode = generateRoomCode();
+     var roomNumber = roomObjects.length;
+     roomObjects[roomNumber] = new RoomObject(roomCode, name);
+     socket.join(roomNumber);
+     io.sockets.in(roomNumber).emit('get-roomcode', roomCode);
+
    });
   socket.on('teacher-disconnect', function(roomNr){
      console.log("The teacher from room number " + roomNr + " has disconnected");
